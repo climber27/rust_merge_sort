cf0401f4148dd0a5c632cdd453b575bc6c4e6374
David Dlima <dlima.david@gmail.com>
2018-10-26 13:42:53 -0400

    simple recursion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
+    let x: i32 = 3;
+    zero(x);
+}
+
+fn zero(mut x: i32) -> bool {
+    println!("x equals {}", x);
+    if x < 0 {
+        x = 0;
+    }
+    if x == 0 {
+        return true;
+    }
+
+    return zero(x-1);
+
 }
