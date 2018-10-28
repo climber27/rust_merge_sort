16615143578697dad38279c38ae3d5329ba63096
David Dlima <dlima.david@gmail.com>
2018-10-28 18:28:28 -0400

    merge sort works

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,51 @@
 fn main() {
-    println!("Hello, world!");
-    let x: i32 = 3;
-    zero(x);
+    println!(" ");
+    println!("Merge Sort in Rust");
+    println!("==================");
+    println!(" ");
+
+    let mut nums = vec![3,1,6,2,4];
+    println!("unsorted: {:?}", nums);
+    merge_sort(&mut nums);
+    println!("sorted: {:?}", nums);
 }
 
-fn zero(mut x: i32) -> bool {
-    println!("x equals {}", x);
-    if x < 0 {
-        x = 0;
-    }
-    if x == 0 {
-        return true;
-    }
+fn merge_sort(nums: &mut std::vec::Vec<usize>) { 
+    if nums.len() > 1 {
+        let mid: usize = nums.len()/2;
+        let mut left: Vec<usize> = nums[0..mid].to_vec();
+        let mut right: Vec<usize> = nums[mid..nums.len()].to_vec();
 
-    return zero(x-1);
+        merge_sort(&mut left);
+        merge_sort(&mut right);
 
+        let mut i: usize = 0;
+        let mut j: usize = 0;
+        let mut k: usize = 0;
+
+        while i < left.len() && j < right.len() {
+            if left[i] < right[j] {
+                nums[k] = left[i];
+                i += 1;
+            } else {
+                nums[k] = right[j];
+                j += 1;
+            }
+            k += 1;
+        }
+
+        while i < left.len() {
+            nums[k] = left[i];
+            i += 1;
+            k += 1;
+        }
+
+        while j < right.len() {
+            nums[k] = right[j];
+            j += 1;
+            k += 1;
+        }
+        
+        
+    }
 }
